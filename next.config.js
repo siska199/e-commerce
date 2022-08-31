42151e262bab23f05c8a610550d5519cded80b06
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-08-31 11:01:35 +0700

    add webpack config (but get comment) to setup playground graphql in next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  // webpack: (config) => {
+  //   if (!config.experiments) {
+  //     config.experiments = {};
+  //   }
+  //   config.experiments.topLevelAwait = true;
+  //   return config;
+  // },
+};

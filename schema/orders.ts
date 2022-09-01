825a096f78b57bdf70bc740428e54624ecfd739d
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-09-01 11:15:36 +0700

    add orderschema

--- schema/orders.ts
+++ schema/orders.ts
@@ -1,9 +1,36 @@
-import { Schema, models, model } from 'mongoose';
+import { Schema, models, model, SchemaTypes } from 'mongoose';
 
 
 
 const OrderSchema = new Schema({
-
+	user : {
+        type : SchemaTypes.ObjectId,
+        required : true
+    },
+	products: [
+		{
+			product : {
+                type : SchemaTypes.ObjectId,
+                required : true
+            },
+			quantity : {type:Number, required :true},
+			options : {
+				color : {type:String, required :true},
+				size : {type:String, required :true}
+			}
+		}		
+	],
+	totalPrice:{
+        type : String,
+        required : true
+    },
+	status : {
+        type : String,
+        enum : {
+            values : ["on prosess", "approved", "rejected"],
+            message : '{VALUE} is not supported'
+        }
+    }
 })
 
 export default models.orders || model("orders", OrderSchema)

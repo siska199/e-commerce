ba52692092e65d46627955f7a2e361030b5517a3
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-09-01 10:51:05 +0700

    add categoryschema

--- schema/categories.ts
+++ schema/categories.ts
@@ -1,9 +1,10 @@
 import { Schema, models, model } from 'mongoose';
 
-
-
 const CartSchema = new Schema({
-
-})
+    name : {
+        type : String,
+        required : [true, `Cart's name shouldn't be empty`],
+    }
+},{timestamps:{createdAt:true, updatedAt:true}})
 
 export default models.categories || model("categories", CartSchema)

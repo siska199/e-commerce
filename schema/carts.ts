9ff56b969a8f6750fdd8ae41bd98d4f9ecedd086
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-09-01 11:18:05 +0700

    add cartschema

--- schema/carts.ts
+++ schema/carts.ts
@@ -1,9 +1,29 @@
-import { Schema, models, model } from 'mongoose';
+import { Schema, models, model, SchemaTypes } from 'mongoose';
 
 
 
 const CartSchema = new Schema({
-
+	user : {
+        type : SchemaTypes.ObjectId,
+        required : true
+    },
+	products: [
+		{
+			product : {
+                type : SchemaTypes.ObjectId,
+                required : true
+            },
+			quantity : {type:Number, required :true},
+			options : {
+				color : {type:String, required :true},
+				size : {type:String, required :true}
+			}
+		}		
+	],
+	totalPrice:{
+        type : String,
+        required : true
+    },
 })
 
 export default models.carts || model("carts", CartSchema)
